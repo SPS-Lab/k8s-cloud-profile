8b182d56b8c48eeabd572cd39833687ee957eaa0
Daniel Medeiros <daniel@n189-p96.eduroam.kth.se>
2023-09-27 15:38:14 +0200

    change kubectl to curl

--- setup-kubernetes-extra.sh
+++ setup-kubernetes-extra.sh
@@ -165,8 +165,9 @@ INFLUXIP=$(cat $OURDIR/influxdb-ip.txt)
 NODE=1
 while [ $NODE -lt $NODECOUNT ]
 do  
-    kubectl exec opencube-influxdb-0 -- influx -execute \""create database node$NODE"\"
-    echo kubectl exec opencube-influxdb-0 -- influx -execute \""create database node$NODE"\"
+    curl --user admin:123456 -XPOST \'http://$INFLUXIP:8086/query\' --data-urlencode \'q=CREATE DATABASE \"node$NODE\"\'
+    #kubectl exec opencube-influxdb-0 -- influx -execute \""create database node$NODE"\"
+    #echo kubectl exec opencube-influxdb-0 -- influx -execute \""create database node$NODE"\"
     ssh node-$NODE "git clone https://github.com/raijenki/kubv2.git && cd kubv2/schemon/ && make && ./njmon_Ubuntu22_aarch64_v81 -I -f -s 30 -i $INFLUXIP -p 8086 -x node$NODE -y admin -z 123456 && exit"
     NODE=$(expr $NODE + 1) 
 done
