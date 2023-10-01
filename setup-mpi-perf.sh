c469e681dcdd08e0101b4a63867eafd3c81cae9a
Daniel Medeiros <daniel@kuriate.com.br>
2023-10-01 17:13:07 +0200

    fixes

--- setup-mpi-perf.sh
+++ setup-mpi-perf.sh
@@ -11,7 +11,7 @@ fi
 
 logtstart "mpi-perf"
 
-maybe_install_packages libopenmpi-dev linux-tools-5.15.0-72 vim
+maybe_install_packages libopenmpi-dev linux-tools-5.15.0-72 vim libopenblas-dev
 $SUDO sysctl kernel.perf_event_paranoid=-1
 $SUDO sh -c " echo 0 > /proc/sys/kernel/kptr_restrict"
 $SUDO cp /lib/linux-tools-5.15.0-72/perf /usr/bin/perf
